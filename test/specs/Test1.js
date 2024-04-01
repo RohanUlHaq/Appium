4c582b1e2b49fdb92761d5f67545c6089a91cffc
Talha Muhammad Ilyas <talhailyas1998@gmail.com>
2024-04-02 03:30:55 +0500

    first commit

---
+++ test/specs/Test1.js
@@ -0,0 +1,17 @@
+describe('Sample', () => {
+    it('Sample test', async() => {
+        await browser.pause(7000); // Add a wait here before interacting with elements
+        await $('~app_tour_footer_btn_next').click();
+        await $('~app_tour_footer_btn_next').click();
+        await $('~app_tour_footer_btn_next').click();
+        await $('~app_tour_footer_btn_next').click();
+        await $('~Menu').click();
+        await $('~menu_btn_login').click();
+        await $('//android.view.ViewGroup[@resource-id="login_switcher"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup').click();
+        await $('~login_input_email').setValue('anser@yopmail.com')
+        await $('~login_btn_next').click();
+        await $('~password_input').setValue('click123');
+        await $('//android.widget.TextView[@text=""]').click();
+        await $('~Next').click();
+    })
+})
